fad42bb75b7aabdd58f9affc34e988968b062a39
majulde <juldediallo18@gmail.com>
2024-12-18 14:23:01 +0000

    second

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Fonction principale pour afficher le message  
 function main() { 
-    echo "Hello, GitHub!";  
+    echo "bonjour, GitHub!";  
     }  
     // Appel de la fonction principale  
     main(); 
